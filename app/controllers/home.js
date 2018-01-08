fec663833e13c2437555e28dd8a16e9b1a2c29f8
Fabio Ferreira da Silva <fabio_ferreiradasilva@yahoo.com.br>
2018-01-08 09:28:32 -0200

    Retirado o log do controller home

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,7 +3,6 @@ module.exports.index = function(application, request, response) {
 	var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
 	noticiasModel.get5UltimasNoticias(function(error, result){
-		console.log(result);
     	response.render('home/index', {'noticias': result});
 	});
 }
