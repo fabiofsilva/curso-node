dbb11b55cd5cb9cedad85787aef683d850964d93
Fabio Ferreira da Silva <fabio_ferreiradasilva@yahoo.com.br>
2018-02-25 13:05:30 -0300

    Ajuste do link home e inclusão de middlewares de páginas de status

--- config/server.js
+++ config/server.js
@@ -34,4 +34,19 @@ consign()
     .then('app/controllers')
 	.into(app);
 
+// middleware que configura páginas de status
+app.use(function(request, response, next){
+	response.status(404).render('erros/404');
+	// Continua o processo
+	next();
+});
+
+// middleware que configura páginas de erro
+app.use(function(erro, request, response, next){
+	response.status(500).render('erros/500');
+	console.log(erro);
+	// Continua o processo
+	next();
+});
+
 module.exports = app;
